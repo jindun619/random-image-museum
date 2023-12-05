36e50359f8fe156ec5ba56a4f67278c1e3822146
jindun619 <jindun619@naver.com>
2023-12-05 14:51:16 +0000

    signin/signup organisms created

--- src/components/atoms/Btn.tsx
+++ src/components/atoms/Btn.tsx
@@ -1,8 +1,19 @@
 function BlockBtn({ text, color }: { text: string; color: string }) {
-  const btnColor = `btn-${color}`
+  const btnColor = `btn-${color}`;
+  const getColorString = (color: string) => {
+    switch (color) {
+      case "neutral":
+        return "btn-neutral";
+      default:
+        return "";
+    }
+  };
+
   return (
     <div>
-      <button className={`btn btn-block ${btnColor}`}>{text}</button>
+      <button className={`btn btn-block ${getColorString(color)}`}>
+        {text}
+      </button>
     </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-import SignIn from "@/components/organisms/SignIn";
+import { SignIn } from "@/components/organisms/SignIn";
+import { SignUp } from "@/components/organisms/SignUp";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY || "";
@@ -8,5 +9,11 @@ const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY || "";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default function IndexPage() {
-  return <SignIn />;
+  return (
+    <>
+      <SignIn />
+      <div className="mt-10"></div>
+      <SignUp />
+    </>
+  );
 }
